acebc643a6ca16a2dd38479f27b9908ce302c3be
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-11-27 16:40:00 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,8 @@
+const { Bot } = require("grammy");
+
+const bot = new Bot(""); // <-- put your bot token between the "" (https://t.me/BotFather)
+
+// Reply to any message with "Hi there!".
+bot.on("message", (ctx) => ctx.reply("Hi there!"));
+
+bot.start();
