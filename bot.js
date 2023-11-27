83c8cffa4396cf6495dc46667df2d8b6a20663f7
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2023-11-27 16:40:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const { Bot } = require("grammy");
 
-const bot = new Bot(""); // <-- put your bot token between the "" (https://t.me/BotFather)
+const bot = new Bot("6916391004:AAGMpzQI9h2kNqdqoyXsOHpZfnYvlfSw-H0");
 
 // Reply to any message with "Hi there!".
 bot.on("message", (ctx) => ctx.reply("Hi there!"));
